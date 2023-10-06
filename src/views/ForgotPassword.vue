fb4918c0d07c1f3fd64da6d57b8b5ce2c937f411
Sara Kozińska <contact@ixtrim.com>
2023-10-06 16:57:23 +0200

    refactor: Changed structure of frontend app. (#1)
    * refactor: Changed structure of frontend app.
    
    * fixup! refactor: Changed structure of frontend app.

--- src/views/ForgotPassword.vue
+++ src/views/ForgotPassword.vue
@@ -1,97 +1,37 @@
 
 <template>
- <div class="forgot-part">
-  <v-iconbox class="v-forgot-key"/>
-<h2 class="text-center">Forgot Password</h2>
-<h4 class="text-center">No worries, we’ll send you reset instructions.</h4>
-  <v-input label="Email" placeholder="Enter your email"/>
+  <div class="forgot-part">
+    <v-iconbox class="v-forgot-key" />
+    <h2 class="text-center">Forgot Password</h2>
+    <h4 class="text-center">No worries, we’ll send you reset instructions.</h4>
+    <v-input label="Email" placeholder="Enter your email" />
 
-   <div class="row v-input-group">
-    <div class="col-lg-6">
-    </div>
-   </div>
-
-   <v-button
-      rounded=true
-      text="Reset password"
-      @click="handleButtonClick"
-      block=true
-    ></v-button>
-     <VLink to="/signin" isRouteLink class="v-button v-button--white v-button--flex v-button--link"><i class="vicon v-back icon-20"></i><span>Back to login</span></VLink>
-    <div class="signup-wrap text-center">
-      <span>Don’t have an account?</span>
-       <VLink to="/signup" isRouteLink>Sign Up</VLink>
-    </div>
- </div>
+    <v-button text="Reset password" @click="handleButtonClick" block=true></v-button>
+    <VLink to="/signin" isRouteLink styled="secondary" icon="left" icon-style="arrow-left" block=true>
+      <span>Back to log in</span>
+    </VLink>
+  </div>
 </template>
 <script>
-import VButton from '@/components/v-button/VButton.vue';
-import VInput from '@/components/v-input/VInput.vue';
-import VLink from '@/components/v-link/VLink.vue';
-import VIconbox from '@/components/v-iconbox/VIconbox.vue';
+  import VButton from '@/components/v-button/VButton.vue';
+  import VInput from '@/components/v-input/VInput.vue';
+  import VLink from '@/components/v-link/VLink.vue';
+  import VIconbox from '@/components/v-iconbox/VIconbox.vue';
 
-export default {
-  components: {
-    VButton,
-    VInput,
-    VLink,
-    VIconbox
-  },
-  methods: {
-    handleButtonClick() {
-      // Handle button click here
+  export default {
+    components: {
+      VButton,
+      VInput,
+      VLink,
+      VIconbox
     },
-  },
-};
+    methods: {
+      handleButtonClick() {
+        // Handle button click here
+      },
+    },
+  };
 </script>
-<style lang="scss">
-@import '@/components/styles/variables/fonts.scss';
-@import '@/components/styles/variables/colors.scss';
-.forgot-part{
-max-width: 360px;
-margin: 0 auto;
-h2{
-  font-family: var(--v-font-inter-semi-bold);
-  color: var(--v-gray-900);
-  font-size: 30px;
-  font-style: normal;
-  font-weight: 600;
-  line-height: 38px; /* 126.667% */
-}
-h4{
-  font-family: var(--v-font-inter-regular);
-  color: var(--v-gray-600);
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 24px; /* 150% */
-  margin-bottom: 32px;
-}
-.input-group{
-  input.mr-8p{
-    margin-right: 8px;
-  }
-  label{
-    color: var(--v-gray-700);
-    font-family: var(--v-font-inter-medium);
-    font-size: 14px;
-    font-style: normal;
-    font-weight: 500;
-    line-height: 20px;
-  }
-}
-.v-input-group{
-  margin-top: 24px;
-  margin-bottom: 24px;
-}
-.signup-wrap{
-  span{
-    font-family: var(--v-font-inter-regular);
-    color: var(--v-gray-600);
-    font-size: 14px;
-    line-height: 20px;
-    margin-right: 8px;
-  }
-}
-}
+<style>
+  @import url(./styles/forgot-password.scss);
 </style>
--- src/views/PasswordChanged.vue
+++ src/views/PasswordChanged.vue
@@ -1,18 +1,14 @@
 
 <template>
- <div class="forgot-part">
-  <v-iconbox class="v-check-circle"/>
-<h2 class="text-center">Password was changed</h2>
-<h4 class="text-center">Your password was successfully changed. You can know use it to log in.</h4>
+  <div class="forgot-part">
+    <v-iconbox class="v-check-circle" />
+    <h2 class="text-center">Password was changed</h2>
+    <h4 class="text-center">Your password was successfully changed. You can know use it to log in.</h4>
 
-   <v-button
-      rounded=true
-      text="Continue"
-      @click="handleButtonClick"
-      block=true
-    ></v-button>
- </div>
+    <v-button text="Continue" @click="handleButtonClick" block=true></v-button>
+  </div>
 </template>
+
 <script>
 import VButton from '@/components/v-button/VButton.vue';
 import VInput from '@/components/v-input/VInput.vue';
@@ -33,54 +29,7 @@ export default {
   },
 };
 </script>
-<style lang="scss">
-@import '@/components/styles/variables/fonts.scss';
-@import '@/components/styles/variables/colors.scss';
-.forgot-part{
-max-width: 360px;
-margin: 0 auto;
-h2{
-  font-family: var(--v-font-inter-semi-bold);
-  color: var(--v-gray-900);
-  font-size: 30px;
-  font-style: normal;
-  font-weight: 600;
-  line-height: 38px; /* 126.667% */
-}
-h4{
-  font-family: var(--v-font-inter-regular);
-  color: var(--v-gray-600);
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 24px; /* 150% */
-  margin-bottom: 32px;
-}
-.input-group{
-  input.mr-8p{
-    margin-right: 8px;
-  }
-  label{
-    color: var(--v-gray-700);
-    font-family: var(--v-font-inter-medium);
-    font-size: 14px;
-    font-style: normal;
-    font-weight: 500;
-    line-height: 20px;
-  }
-}
-.v-input-group{
-  margin-top: 24px;
-  margin-bottom: 24px;
-}
-.signup-wrap{
-  span{
-    font-family: var(--v-font-inter-regular);
-    color: var(--v-gray-600);
-    font-size: 14px;
-    line-height: 20px;
-    margin-right: 8px;
-  }
-}
-}
+
+<style>
+  @import url(./styles/password-changed.scss);
 </style>

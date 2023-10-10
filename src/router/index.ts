3f217f2f4433b6f9d09274ffa812dbd471aba2a6
ixtrim <contact@ixtrim.com>
2023-10-10 17:36:37 +0200

    feat: App global left navigation.

--- src/router/index.ts
+++ src/router/index.ts
@@ -55,6 +55,31 @@ const router = createRouter({
       path: '/settings',
       name: 'settings',
       component: () => import('../views/Settings.vue')
+    },
+    {
+      path: '/settings-team',
+      name: 'settings-team',
+      component: () => import('../views/SettingsTeam.vue')
+    },
+    {
+      path: '/settings-notifications',
+      name: 'settings-notifications',
+      component: () => import('../views/SettingsNotifications.vue')
+    },
+    {
+      path: '/settings-invoice',
+      name: 'settings-invoice',
+      component: () => import('../views/SettingsInvoice.vue')
+    },
+    {
+      path: '/settings-calendar',
+      name: 'settings-calendar',
+      component: () => import('../views/SettingsCalendar.vue')
+    },
+    {
+      path: '/settings-billing',
+      name: 'settings-billing',
+      component: () => import('../views/SettingsBilling.vue')
     }
   ]
 })

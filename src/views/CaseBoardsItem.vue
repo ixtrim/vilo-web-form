5ae2e0041f0ac8929dae2b8b5d18dd1c514f1cf4
ixtrim <contact@ixtrim.com>
2023-12-01 20:12:48 +0100

    Edit case board frontend built.

--- src/views/CaseBoardsItem.vue
+++ src/views/CaseBoardsItem.vue
@@ -15,7 +15,7 @@
         </div>
       </div>
       <div class="col-lg-2 align-right">
-        <v-button :block="false" size="sm" icon="left" icon-style="edit--2" styled="outlined" @click="handleButtonClick" text="Edit"></v-button>
+        <v-button :block="false" size="sm" icon="left" icon-style="edit--2" styled="outlined" @click="openEditModal" text="Edit"></v-button>
       </div>
     </div>
 
@@ -31,6 +31,12 @@
       </div>
     </div>
 
+    <VModal :show="showEditModal" :title="modalEditTitle" @update:show="handleModalClose">
+      <VEditCaseBoard v-if="showEditModal" :title="modalEditTitle" @close-modal="showEditModal = false" @save-clicked="handleEditCase" />
+    </VModal>
+
+    <VNotification ref="notificationRef" :type="notificationType" :header="notificationHeader" :message="notificationMessage" :duration="7000" />
+
   </div>
 </template>
 
@@ -40,16 +46,31 @@
   import VButton from '@/components/v-button/VButton.vue';
   import VModalSmall from '@/components/v-modal-small/v-modal-small.vue';
   import Board from '@/modules/Board.vue';
+  import VNotification from '@/components/v-notification/VNotification.vue';
+  import VModal from '@/components/v-modal/v-modal.vue';
+  import VEditCaseBoard from '@/modals/CaseBoards/v-edit-case-board/v-edit-case-board.vue';
+
+  interface NotificationRef {
+    showNotification: () => void;
+  }
 
   export default defineComponent({
     components: {
       VBreadcrumbs,
       VButton,
       VModalSmall,
+      VNotification,
+      VModal,
+      VEditCaseBoard,
       Board
     },
     data() {
       return {
+        showEditModal: false,
+        modalEditTitle: '',
+        notificationType: 'success',
+        notificationHeader: 'Changes saved',
+        notificationMessage: 'This account has been successfully edited.',
         breadcrumbs: [
           { text: 'Case boards', to: '/case-boards' },
           { text: 'Example Vilo Board' }
@@ -57,8 +78,22 @@
       };
     },
     methods: {
-      handleButtonClick() {
-        
+      triggerNotification(type: string, header: string, message: string) {
+        this.notificationType = type;
+        this.notificationHeader = header;
+        this.notificationMessage = message;
+        (this.$refs.notificationRef as NotificationRef).showNotification();
+      },
+      openEditModal() {
+        this.modalEditTitle = 'Edit case board';
+        this.showEditModal = true;
+      },
+      handleEditCase() {
+        this.showEditModal = false;
+        this.triggerNotification('success', 'Changes saved', 'Case board modified successfully.');
+      },
+      handleModalClose(value: boolean) {
+        this.showEditModal = false;
       },
     },
   });

f232aab6682174642e71a631774edf295d5c9af0
ixtrim <contact@ixtrim.com>
2024-01-21 21:01:02 +0100

    Temp Google Calendar

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -8,14 +8,14 @@
           <p> Create and manage your meetings and events.</p>
         </div>
       </div>
-      <div class="col-lg-2">
+      <div class="col-lg-2" style="display: none;">
         <VButton :block="true" size="md" icon="left" icon-style="add-white" @click="handleButtonClick" text="Add new event"></VButton>
       </div>
     </div>
 
     <div class="row fill-space">
       <div class="col-lg-12">
-        
+        <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=Europe%2FMadrid&src=c2tvemluc2thd2ViQGdtYWlsLmNvbQ&src=Y2Q3NDhjODliZmNlNmQyY2E1YTZkM2Y3ZTZhYWU5OWQ1ZmI0ZWI5YWFjMTQ0ZmMxYjcxZjU2NTFhYTIxZjI2OEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=OHVnM3NoN3FpbDVlODlkdDYyNzJuajBiMWtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=b3RhMGRwdTNwdXEzdWduNDQxcmd2YmN1dmNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4uc3BhaW4jaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23039BE5&color=%23AD1457&color=%239E69AF&color=%23E4C441&color=%2333B679&color=%230B8043" style="border-width:0" width="100%" height="600" frameborder="0" scrolling="no"></iframe>
       </div>
     </div>
 

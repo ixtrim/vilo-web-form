cbf67b755de5bd07cefdf64ad3c324de60756f8b
ixtrim <contact@ixtrim.com>
2024-02-19 19:34:41 +0100

    Reverted Navigation

--- src/modules/Home/TotalIncome/TotalIncome.vue
+++ src/modules/Home/TotalIncome/TotalIncome.vue
@@ -6,7 +6,9 @@
       </div>
     </div>
     <div class="row">
-      
+      <div class="col-lg-12 align-center">
+        <img src="@/assets/temporary/income-chart.svg" alt="Total income" />
+      </div>
     </div>
   </div>
 </template>

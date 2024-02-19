c2648b41e616ff47bbd43ff15158e7a4d68661ed
ixtrim <contact@ixtrim.com>
2024-02-19 18:51:39 +0100

    TEMP: Removing placeholders

--- src/modules/Home/RecentInvoices/RecentInvoices.vue
+++ src/modules/Home/RecentInvoices/RecentInvoices.vue
@@ -11,41 +11,7 @@
     <div class="row">
       <div class="col-lg-12 align-center">
 
-        <div class="recent-invoices__item">
-          <div class="recent-invoices__item__number">
-            <h4>INV-3066</h4>
-          </div>
-          <div class="recent-invoices__item__user">
-            <VUser userName="Olivia Rhye" userEmail="olivia@untitledui.com" />
-          </div>
-          <div class="recent-invoices__item__status">
-            <VBadge variant="warning">Pending</VBadge>
-          </div>
-        </div>
-
-        <div class="recent-invoices__item">
-          <div class="recent-invoices__item__number">
-            <h4>INV-3065</h4>
-          </div>
-          <div class="recent-invoices__item__user">
-            <VUser userName="Phoenix Baker" userEmail="phoenix@untitledui.com" />
-          </div>
-          <div class="recent-invoices__item__status">
-            <VBadge variant="warning">Pending</VBadge>
-          </div>
-        </div>
-
-        <div class="recent-invoices__item">
-          <div class="recent-invoices__item__number">
-            <h4>INV-3064</h4>
-          </div>
-          <div class="recent-invoices__item__user">
-            <VUser userName="Lana Steiner" userEmail="lana@untitledui.com" />
-          </div>
-          <div class="recent-invoices__item__status">
-            <VBadge variant="light">Draft</VBadge>
-          </div>
-        </div>
+        
         
       </div>
     </div>
--- src/modules/Home/UpcomingMeetings/UpcomingMeetings.vue
+++ src/modules/Home/UpcomingMeetings/UpcomingMeetings.vue
@@ -11,29 +11,7 @@
     <div class="row">
       <div class="col-lg-12">
 
-        <div class="upcoming-meetings__item upcoming-meetings__item--blue">
-          <div class="upcoming-meetings__item__details">
-            <h4>Vilo Task Planning</h4>
-            <div class="upcoming-meetings__item__details__members">
-              <img src="@/assets/temporary/meeting_members-1.png" alt="" />
-            </div>
-          </div>
-          <div class="upcoming-meetings__item__date">
-            <p>Today, 10:15 - 10:45</p>
-          </div>
-        </div>
-
-        <div class="upcoming-meetings__item upcoming-meetings__item--green">
-          <div class="upcoming-meetings__item__details">
-            <h4>Consultation with Jane Kovalsky</h4>
-            <div class="upcoming-meetings__item__details__members">
-              <img src="@/assets/temporary/meeting_members-2.png" alt="" />
-            </div>
-          </div>
-          <div class="upcoming-meetings__item__date">
-            <p>Today, 10:15 - 10:45</p>
-          </div>
-        </div>
+        
         
       </div>
     </div>

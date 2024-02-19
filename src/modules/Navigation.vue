030f2e547f34743e12891a69aa7bd6b2e67ab9de
ixtrim <contact@ixtrim.com>
2024-02-19 18:27:58 +0100

    Settings link visible only for admin user.

--- src/modules/Navigation.vue
+++ src/modules/Navigation.vue
@@ -19,7 +19,7 @@
       </div>
     </div>
 
-    <div class="row">
+    <div class="row" v-if="userInfo.role === 0">
       <div class="col-lg-12">
         <Settings />
       </div>
@@ -35,11 +35,25 @@
 </template>
 
 <script setup lang="ts">
+  import { computed, ref, onMounted } from 'vue';
+  import { getAuth, signOut } from 'firebase/auth';
+  import { useUserStore } from '@/stores/userStore';
+  import { useRouter } from 'vue-router';
+
   import Logo     from '@/modules/Navigation/Logo.vue';
-  import Search   from '@/modules/Navigation/Search.vue';
   import Menu     from '@/modules/Navigation/Menu.vue';
   import Settings from '@/modules/Navigation/Settings.vue';
   import User     from '@/modules/Navigation/User.vue';
+
+  const auth = getAuth();
+  const { user } = useUserStore();
+  const router = useRouter();
+
+  const userInfo = computed(() => {
+    return {
+      role: user.value?.role ?? '',
+    };
+  });
 </script>
 
 <style>

6dc1269ab8cfade1af6d17b1df93920813017b44
ixtrim <contact@ixtrim.com>
2024-02-29 08:41:00 +0100

    WIP: Case Edition. Client not working.

--- src/modals/CaseBoards/v-edit-case-board/v-edit-case-board.vue
+++ src/modals/CaseBoards/v-edit-case-board/v-edit-case-board.vue
@@ -25,7 +25,7 @@
         <VTextarea 
           label="Description" 
           placeholder="e.g. I joined Stripe’s Customer Success team to help them scale their checkout product. I focused mainly on onboarding new customers and resolving complaints." 
-          v-model="computedUserNotes"
+          v-model="localDescription"
         />
       </div>
     </div>
@@ -62,13 +62,6 @@
   import VTextarea from '@/components/v-textarea/v-textarea.vue';
   import VDropdown from '@/components/v-dropdown/VDropdown.vue';
   import VButton from '@/components/v-button/VButton.vue';
-
-  const localUserName = ref('Smith v. Jones, New York State Court');
-  const localUserEmail = ref('');
-  const localUserCompany = ref('');
-  const localUserPhone = ref('');
-  const localUserAddress = ref('');
-  const localUserPosition = ref('');
 
   const localTitle = ref('');
   const localDescription = ref('');

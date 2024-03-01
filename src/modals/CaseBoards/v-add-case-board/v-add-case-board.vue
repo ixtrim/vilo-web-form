e0dbefa6f8b46fb2ab50c8a349b14941a7e19c5d
ixtrim <contact@ixtrim.com>
2024-03-01 14:03:27 +0100

    Cases Working and built.

--- src/modals/CaseBoards/v-add-case-board/v-add-case-board.vue
+++ src/modals/CaseBoards/v-add-case-board/v-add-case-board.vue
@@ -43,10 +43,10 @@
   <div class="modal-footer">
     <ul class="modal-footer__actions">
       <li>
-        <v-button :block="false" size="md" styled="outlined" @click="closeModal" text="Close"></v-button>
+        <v-button :block="false" size="md" styled="outlined" icon="left" icon-style="save" @click="saveAsDraft" text="Save as draft"></v-button>
       </li>
       <li>
-        <v-button :block="false" size="md" styled="Primary" @click="saveAndClose" text="Save"></v-button>
+        <v-button :block="false" size="md" styled="Primary" @click="saveAndClose" text="Create a case"></v-button>
       </li>
     </ul>
   </div>
@@ -88,6 +88,26 @@
       value: doc.id
     }));
   };
+
+  const iconUrls = [
+    'https://firebasestorage.googleapis.com/v0/b/vilo-ebc86.appspot.com/o/cases_icons%2FAvatar-2.svg?alt=media&token=e79a0a6d-8421-4993-b152-ee542fd7e0ed',
+    'https://firebasestorage.googleapis.com/v0/b/vilo-ebc86.appspot.com/o/cases_icons%2FAvatar-3.svg?alt=media&token=e87795eb-7045-40a1-94d7-59266b90973b',
+    'https://firebasestorage.googleapis.com/v0/b/vilo-ebc86.appspot.com/o/cases_icons%2FAvatar-4.svg?alt=media&token=409fa9e4-efdf-4dfd-8d06-92accc7efb4f',
+    'https://firebasestorage.googleapis.com/v0/b/vilo-ebc86.appspot.com/o/cases_icons%2FAvatar-5.svg?alt=media&token=04e731e6-cdba-4429-81ea-6ad753b2743c',
+    'https://firebasestorage.googleapis.com/v0/b/vilo-ebc86.appspot.com/o/cases_icons%2FAvatar.svg?alt=media&token=5b5f5742-716b-4637-86c1-ba4bc5279c17',
+    'https://firebasestorage.googleapis.com/v0/b/vilo-ebc86.appspot.com/o/cases_icons%2Fanalysis.png?alt=media&token=51c3b46b-a970-4d38-a30e-bec1e43f5864',
+    'https://firebasestorage.googleapis.com/v0/b/vilo-ebc86.appspot.com/o/cases_icons%2Fsuitcase.png?alt=media&token=149c77dd-ebc6-4544-8888-64f6c9bedcb6',
+    'https://firebasestorage.googleapis.com/v0/b/vilo-ebc86.appspot.com/o/cases_icons%2Fstudy.png?alt=media&token=5d0cdb10-1b7a-4bdf-b0cc-59ca8bedae31',
+    'https://firebasestorage.googleapis.com/v0/b/vilo-ebc86.appspot.com/o/cases_icons%2Fprotective.png?alt=media&token=d3b62eca-2409-476d-97fe-a5bd7b76ee64',
+    'https://firebasestorage.googleapis.com/v0/b/vilo-ebc86.appspot.com/o/cases_icons%2Flayers.png?alt=media&token=7ee756de-b1e4-46de-8fd0-86d42f481876',
+    'https://firebasestorage.googleapis.com/v0/b/vilo-ebc86.appspot.com/o/cases_icons%2Flayer.png?alt=media&token=92ba9229-13f4-4f96-8a97-655b3f4ec208',
+    'https://firebasestorage.googleapis.com/v0/b/vilo-ebc86.appspot.com/o/cases_icons%2Fgavel.png?alt=media&token=e545238e-4d84-4743-b39a-9059c62cfdea',
+    'https://firebasestorage.googleapis.com/v0/b/vilo-ebc86.appspot.com/o/cases_icons%2Fcase.png?alt=media&token=9b580423-3b72-4a73-9548-c8e120874f35',
+  ];
+  function getRandomIconUrl() {
+    const randomIndex = Math.floor(Math.random() * iconUrls.length);
+    return iconUrls[randomIndex];
+  }
 
   const allUsers: Ref<DropdownItem[]> = ref([]);
   const selectedTeamMembers: Ref<DropdownItem[]> = ref([]);
@@ -144,6 +164,26 @@
     emit('close-modal');
   }
 
+  async function saveAsDraft(event: any) {
+    event.stopPropagation();
+    try {
+      await addDoc(collection(db, "cases"), {
+        title: localTitle.value,
+        description: localDescription.value,
+        client_id: localClient.value === '0' ? '' : localClient.value,
+        team_members: selectedTeamMembers.value.map(member => member.value),
+        time_added: new Date(),
+        status: 0,
+        icon: 'https://firebasestorage.googleapis.com/v0/b/vilo-ebc86.appspot.com/o/cases_icons%2Fcase-draft.png?alt=media&token=d65fe279-2ac4-4643-bab4-ae755903c19d',
+      });
+
+      emit('save-clicked');
+      closeModal();
+    } catch (error) {
+      console.error("Failed to add new case:", error);
+    }
+  }
+
   async function saveAndClose(event: any) {
     event.stopPropagation();
     try {
@@ -154,7 +194,7 @@
         team_members: selectedTeamMembers.value.map(member => member.value),
         time_added: new Date(),
         status: 1,
-        icon: 'https://firebasestorage.googleapis.com/v0/b/vilo-ebc86.appspot.com/o/cases_icons%2FAvatar-5.svg?alt=media&token=04e731e6-cdba-4429-81ea-6ad753b2743c'
+        icon: getRandomIconUrl(),
       });
 
       emit('save-clicked');

2bf827c1c48dff5957411149957ecdeab2fa9cf7
ixtrim <contact@ixtrim.com>
2024-03-06 13:21:58 +0100

    Google Calendar Integration Test V3

--- src/modals/Invoices/v-add-invoice/v-add-invoice.vue
+++ src/modals/Invoices/v-add-invoice/v-add-invoice.vue
@@ -3,7 +3,7 @@
 
     <div class="row">
       <div class="col-lg-8">
-        <h4 class="add-invoice__nr">Invoice <span>#3066</span></h4>
+        <h4 class="add-invoice__nr">Invoice <span></span></h4>
       </div>
       <div class="col-lg-4">
         
@@ -37,19 +37,19 @@
       </div>
     </div>
 
-    <div class="row">
+    <div class="row" style="display:none;">
       <div class="col-lg-12">
         <img src="@/assets/invoice-1.svg" alt="Vilo Icon" style="width: 100%;">
       </div>
     </div>
 
-    <div class="row mt-3">
+    <div class="row" style="display:none;">
       <div class="col-lg-12">
         <img src="@/assets/invoice-3.svg" alt="Vilo Icon" style="width: 100%;">
       </div>
     </div>
 
-    <div class="row mt-2">
+    <div class="row" style="display:none;">
       <div class="col-lg-12">
         <img src="@/assets/invoice-3.svg" alt="Vilo Icon" style="width: 100%;">
       </div>
@@ -97,7 +97,7 @@
 
     <div class="row">
       <div class="col-lg-12">
-        <p>Total: <strong>$1400</strong></p>
+        <p>Total: <strong></strong></p>
       </div>
     </div>
 
